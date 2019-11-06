474d7e8fee33214f6023228c0e2b3c76685c86c2
Norman Tang <norman@normstorm.com>
2019-11-06 10:10:11 -0600

    setup initialization script

---
+++ initialize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git config --global user.email "norman@normstorm.com"
+git config --global user.name "Norman Tang"
+cp ~/configuration/.vimrc ~/
+cp ~/configuration/.bashrc ~/
+cp ~/configuration/.inputrc ~/
+cp ~/configuration/doupdate.sh ~/
+chmod u+x ~/doupdate.sh
+
