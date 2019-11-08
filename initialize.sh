431b49d267f7aeb72ca8b3638d46dede4876ac88
Norman Tang <norman@normstorm.com>
2019-11-08 02:54:53 -0600

    added flatpak

--- initialize.sh
+++ initialize.sh
@@ -16,5 +16,11 @@ cd ~/
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update -y
 sudo apt install python3.8 -y
 sudo apt install gnome-tweaks  # need this for setting up right mouse click on laptop trackpad
+sudo snap install htop 
+sudo apt install materia-gtk-theme -y
+sudo add-apt-repository ppa:alexlarsson/flatpak
+sudo apt-get update -y && sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
