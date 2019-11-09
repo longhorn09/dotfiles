3b7c5160c9ce91253cc73b710a83ff3274949d0b
Norman Tang <norman@normstorm.com>
2019-11-08 20:56:16 -0600

    updated comment for vbox

--- initialize.sh
+++ initialize.sh
@@ -54,7 +54,7 @@ pip install edgegrid-python  # https://github.com/akamai/AkamaiOPEN-edgegrid-pyt
 sudo apt install httpie -y
 pip install httpie-edgegrid  # better to git clone the source repo and  build than do this: https://github.com/akamai/httpie-edgegrid
 
-# virtualbox installation
+# virtualbox installation - note: you'll likely need to reboot to be able to run virtualbox
 sudo apt install virtualbox-dkms -y
 sudo apt-get install virtualbox -y
 sudo apt install virtualbox-ext-pack -y
