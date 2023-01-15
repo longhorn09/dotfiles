a656f43a8baf56872deb523214864fe7e224a757
Norman Tang <norman@normstorm.com>
2023-01-14 23:18:20 -0500

    addd commands for setting up gcloud CLI

--- initialize.sh
+++ initialize.sh
@@ -78,6 +78,13 @@ sudo apt install default-jdk -y
 sudo apt install zip unzip
 curl -s https://get.sdkman.io | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# setup for gcloud CLI - https://cloud.google.com/sdk/docs/install#deb
+sudo apt-get update -y && sudo apt-get upgrade -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
+sudo apt-get update -y && sudo apt-get install google-cloud-cli -y
+
 # final refresh and cleanup
 sudo apt dist-upgrade -y
 sudo apt-get upgrade -y
