3322b5007e38609b0238b8b01c45874ca360e8ad
Norman Tang <norman@normstorm.com>
2023-01-25 21:26:07 -0500

    added new script for desktop software initialization

---
+++ desktop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install mysql workbench
+sudo apt-get update -y
+snap install mysql-workbench-community -y
+
+# download chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+# VSCode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+#sudo apt install code -y
+sudo snap install --classic code
