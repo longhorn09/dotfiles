d9258a1bcae2de9964138c33a883135f3e398771
Norman Tang <norman@normstorm.com>
2023-01-28 17:55:40 -0500

    added dotfiles copy to home directory

--- initialize.sh
+++ initialize.sh
@@ -112,3 +112,11 @@ sudo apt autoremove -y
 chmod u+x ./desktop.sh
 curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# copy dotfiles
+cp .vimrc ~/
+cp .bashrc ~/
+cp .inputrc ~/
+source ~/.vimrc
+source ~/.bashrc
+source ~/.inputrc
