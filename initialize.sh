5a606b731a555134a14e0ef2f7818e61451a97ba
Norman Tang <norman@normstorm.com>
2023-01-28 21:39:40 -0500

    install mysql client core

--- initialize.sh
+++ initialize.sh
@@ -113,6 +113,9 @@ chmod u+x ./desktop.sh
 curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# mysql client
+sudo apt install mysql-client-core-8.0 -y
+
 # copy dotfiles
 cp .vimrc ~/
 cp .bashrc ~/
