d0986e942b9be621bb6cb631d79097c6066dcf15
Norman Tang <norman@normstorm.com>
2024-01-28 15:38:55 -0500

    added chrome

--- doinstall.sh
+++ doinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo apt-get update -y
 sudo snap install notepadqq 
 sudo snap install code
 sudo snap install google-cloud-cli --classic
-
+sudo apt-get install google-chrome-stable -y
