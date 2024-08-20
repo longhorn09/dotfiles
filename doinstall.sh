0bf48446e2979ef1137c613e2dcc9e1e953d44b0
Norman Tang <norman@normstorm.com>
2024-08-19 22:19:24 -0400

    install virtualbox

--- doinstall.sh
+++ doinstall.sh
@@ -12,3 +12,6 @@ sudo snap install gnome-calculator
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install
+
+# virtualbox
+sudo apt install virtualbox virtualbox-ext-pack virtualbox-qt
