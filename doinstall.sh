8f62ed14802fe463f5fc5e8d6cf6dd9c359cb2f5
Norman Tang <norman@normstorm.com>
2024-08-19 21:57:42 -0400

    chrome install

--- doinstall.sh
+++ doinstall.sh
@@ -3,7 +3,12 @@ sudo apt-get update -y
 sudo snap install notepadqq 
 sudo snap install code
 sudo snap install google-cloud-cli --classic
-sudo apt-get install google-chrome-stable -y
+#sudo apt-get install google-chrome-stable -y
 sudo snap install discord
 sudo snap install gimp
 sudo snap install gnome-calculator
+
+# google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
