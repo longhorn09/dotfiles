e67b1f62f129ca1da9640f1f2d20b831ad322289
Norman Tang <norman@normstorm.com>
2024-08-19 22:58:48 -0400

    added comments

--- doinstall.sh
+++ doinstall.sh
@@ -14,4 +14,6 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install
 
 # virtualbox
+# https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm
 sudo apt install virtualbox virtualbox-ext-pack virtualbox-dkms virtualbox-qt virtualbox-guest-additions-iso -y
+
