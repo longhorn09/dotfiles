1b5d8b1f27f1f0c5da61faa10f8bb0412b595143
Norman Tang <norman@normstorm.com>
2025-07-22 17:44:48 -0400

    1Password install scripts

--- doinstall.sh
+++ doinstall.sh
@@ -81,3 +81,12 @@ sudo apt-get install -f
 sudo apt install xdotool -y
 sudo apt install flameshot -y   # https://github.com/flameshot-org/flameshot/issues/3712#issuecomment-2334966021
 # make a shortcut with : sh -c "QT_QPA_PLATFORM=wayland flameshot gui"
+
+# 1Password install
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --batch --yes --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --batch --yes --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo apt update -y && sudo apt install 1password -y
