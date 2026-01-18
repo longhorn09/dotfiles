7275c1e3f69290f98cc202e7592fbde54d28e9a9
Norman Tang <norman@normstorm.com>
2026-01-17 19:50:02 -0500

    added snap install of 1password

--- doinstall.sh
+++ doinstall.sh
@@ -13,7 +13,7 @@ sudo snap install vlc
 sudo apt install software-properties-common apt-transport-https wget -y
 #wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-
+sudo snap install 1password
 #sudo snap install notepadqq 
 sudo add-apt-repository ppa:notepadqq-team/notepadqq -y
 #sudo apt install notepadqq -y
