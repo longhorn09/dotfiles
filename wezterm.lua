172e113466388658bebb65c2a50405f2c56243a1
Norman Tang <norman@normstorm.com>
2026-08-23 02:12:44 -0400

    some wezterm setup

--- wezterm.lua
+++ wezterm.lua
@@ -5,4 +5,23 @@ local config = wezterm.config_builder()
 config.front_end = "WebGpu" 
 config.enable_wayland = false
 
+config.initial_cols = 130  -- Increase this number to make it wider (default is usually 80)
+config.initial_rows = 40   -- Adjust height as needed
+
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+config.keys = {
+  -- Split horizontally (Leader + %)
+  {
+    key = '%',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  -- Split vertically (Leader + ")
+  {
+    key = '"',
+    mods = 'LEADER|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
 return config
